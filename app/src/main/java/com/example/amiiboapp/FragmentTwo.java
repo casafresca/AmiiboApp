1c65b0bce587c74ed81b8408fcdcea15adf7411a
Jordan <Jordan.waldron94@gmail.com>
2020-05-07 15:31:53 -0400

    testing fragments

--- app/src/main/java/com/example/amiiboapp/FragmentTwo.java
+++ app/src/main/java/com/example/amiiboapp/FragmentTwo.java
@@ -59,6 +59,6 @@ public class FragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_Two, container, false);
+        return inflater.inflate(R.layout.fragment_two, container, false);
     }
 }
